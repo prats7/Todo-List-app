2833ed72d33daa964151a1fb6c049f39857a47e9
prats7 <pratyushvishal04@gmail.com>
2020-12-12 15:32:01 +0530

    Add todo

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const taskButton = document.getElementById("todo-button");
-const taskInput = document.getElementById("todo-input");
-const todoList = document.querySelector(".todo-list");
+//Select DOM
+const todoInput = document.getElementById("todo-input");
+const todoButton = document.getElementById("todo-button");
+const todoList = document.getElementById("todo-list");
+const tasksCounter = document.getElementById('tasks-counter');
 
+let todos = [];
 
-
+function addTodos(todos){
+    const list = document.createElement('list');
+    list.innerHTML = `<label for="${todos.id}">${todos.text}</label>
+    <input type="checkbox" id="${todos.id}" ${todos.done?'checked':''} class="checkbox">
+    <img src="bin.svg" class="delete" data-id="${task.id}"/>
+    `;
+    todoList.appendChild(list);
+}
